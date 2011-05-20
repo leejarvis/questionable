367edeadae5ad4cb80bf2d67ecd6546543383d53
Lee Jarvis <lee@jarvis.co>
2011-05-20 10:36:39 +0100

    added doc example to questionable method

--- lib/questionable.rb
+++ lib/questionable.rb
@@ -9,6 +9,40 @@ module Questionable
     klass.extend self
   end
 
+  # Add questionable items
+  #
+  # @example
+  #   class Person
+  #     include Questionable
+  #
+  #     questionable :name, :age
+  #     questionable :has_password? => :password
+  #
+  #     questionable :friends_with_robert? => :friends do |friends|
+  #       friends.include? 'Robert'
+  #     end
+  #
+  #     questionable :has_friend? => :friends do |friends, friend|
+  #       friends.include? friend
+  #     end
+  #
+  #     def initialize(name, age, password=nil)
+  #       @name = name
+  #       @age = age
+  #       @password = password
+  #       @friends = ['Phil', 'Robert']
+  #     end
+  #   end
+  #
+  #   roy = Person.new 'Roy', 36, 'Sekret!!'
+  #   roy.name? #=> true
+  #   roy.has_password? #=> true
+  #   roy.friends_with_robert? #=> true
+  #
+  #   dave = Person.new 'Dave', 42
+  #   dave.age? #=> true
+  #   dave.has_password? #=> false
+  #   dave.has_friend? 'Steve' #=> false
   def questionable(*items, &block)
     if items.first.respond_to? :keys
       __create_questionable_method *items[0].shift, &block
